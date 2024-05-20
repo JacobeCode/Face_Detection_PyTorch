90798c624c20f47cdeeea54814d569a9a2d147df
Jakub Gucik <kuba.g.code.4@gmail.com>
2024-05-20 19:43:16 +0200

    Currently - Dummy model

--- emotions_model_training/ERNN.py
+++ emotions_model_training/ERNN.py
@@ -1,5 +1,24 @@
-from torch.nn import Module
+from torch import nn
 
-class ERNN(Module):
+# Currently dummy network for checking if training and loaders are working properly
+
+class ERNN(nn.Module):
     def __init__(self):
         super().__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=1, kernel_size=(197, 197), stride=120)
+        self.relu1 = nn.ReLU()
+
+        self.flatten = nn.Flatten()
+
+        self.lin1 = nn.Linear(289, 7)
+        self.soft = nn.Softmax()
+
+    def forward(self, x):
+        hidden = None
+
+        x = self.relu1(self.conv1(x))
+        x = self.flatten(x)
+        x = self.soft(self.lin1(x))
+        
+        return x
