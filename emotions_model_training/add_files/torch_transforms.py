988cfe01739ee4ff666db2b1c0ddac5a01e53b68
Jakub Gucik <kuba.g.code.4@gmail.com>
2024-05-23 19:18:51 +0200

    Init for transforms container

---
+++ emotions_model_training/add_files/torch_transforms.py
@@ -0,0 +1,10 @@
+import torch
+
+from torchvision.transforms import v2
+
+import torchvision.transforms as ttinter
+import numpy as np
+
+class torch_transforms:
+    def __init__(self):
+        self.transforms = None
