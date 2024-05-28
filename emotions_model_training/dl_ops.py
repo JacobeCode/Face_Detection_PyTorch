53b6fe5a7223e601f10965b533a2415208b965e5
Jakub Gucik <kuba.g.code.4@gmail.com>
2024-05-28 17:59:45 +0200

    Added MLFlow metrics handling

--- emotions_model_training/dl_ops.py
+++ emotions_model_training/dl_ops.py
@@ -1,11 +1,21 @@
+import mlflow.types
+import torch
+import mlflow
+
+from mlflow.models import infer_signature
+
 class dl_ops():
     def __init__(self, model, loss_fn, optimizer):
         self.model = model
         self.loss_fn = loss_fn
         self.optimizer = optimizer
+
     def train(self, dataloader, device):
         # Setting proper mode
         self.model.train()
+
+        signature = None
+        input_example = None
 
         for iteration, batch in enumerate(dataloader):
             sample = batch['image'].to(device)
@@ -21,15 +31,33 @@ class dl_ops():
             loss_item, current = loss.item(), (iteration + 1) * len(sample)
             print(f"Batch: {iteration + 1} ({current / len(dataloader.dataset)}) \n Loss: {loss_item:>3f}")
 
-    def valid(self, dataloader, device):
+            mlflow.log_metric("train_loss", loss_item, iteration)
+
+    def valid(self, dataloader, device, epoch):
         self.model.eval()
+
+        correct_pred=0
+        accuracy = 0
+        valid_loss = 0
 
         for iteration, batch in enumerate(dataloader):
             sample = batch['image'].to(device)
             label = batch['label'].to(device)
 
             pred = self.model(sample)
-            loss = self.loss_fn(pred, label)
+            valid_loss = self.loss_fn(pred, label)
+            
+            correct_pred += (pred.argmax(1) == label).type(torch.float).sum().item()
+
+        valid_loss /= len(dataloader)
+        accuracy = correct_pred / len(dataloader.dataset)
+        accuracy *= 100
+
+        mlflow.log_metric("accuracy_val", accuracy, epoch)
+        mlflow.log_metric("loss_val", valid_loss, epoch)
+        mlflow.log_metric("correct_pred", correct_pred, epoch)
+
+        print(f"Validation Loss: {valid_loss:>3f} % \n Accuracy of validation: {accuracy:>3f}")
 
     def test(self):
         self.model.test()
