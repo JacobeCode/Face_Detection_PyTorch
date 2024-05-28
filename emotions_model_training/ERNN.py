4ced164cbdf3f9df4c04a4c1ead3e1d687f7d1a9
Jakub Gucik <kuba.g.code.4@gmail.com>
2024-05-28 18:00:15 +0200

    First ready build of ERNN

--- emotions_model_training/ERNN.py
+++ emotions_model_training/ERNN.py
@@ -3,22 +3,53 @@ from torch import nn
 # Currently dummy network for checking if training and loaders are working properly
 
 class ERNN(nn.Module):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, block, layers, num_classes = 7):
+        super(ERNN, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=1, kernel_size=(20, 20), stride=10)
-        self.relu1 = nn.ReLU()
+        self.inplanes = 64
+        # A little larger kernel to test on slightly larger images
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
+        self.maxpool1 = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
 
-        self.flatten = nn.Flatten()
+        self.resblock1 = self.make_layer(block, 64, layers[0], stride=1)
+        self.resblock2 = self.make_layer(block, 128, layers[1], stride=2)
+        self.resblock3 = self.make_layer(block, 256, layers[2], stride=2)
+        self.resblock4 = self.make_layer(block, 512, layers[3], stride=2)
 
-        self.lin1 = nn.Linear(576, 7)
-        self.soft = nn.Softmax()
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.flat = nn.Flatten()
+        self.fullcon = nn.Linear(2048, 7)
+        self.softmax = nn.Softmax()
+
+
+    def make_layer(self, block, planes, num_blocks, stride=1):
+        resiudal_val = None
+        if stride != 1 or self.inplanes != planes:
+            resiudal_val = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(planes),
+            )
+        layers = []
+        layers.append(block(self.inplanes, planes, stride=stride, residual=resiudal_val))
+        self.inplanes = planes
+        for i in range(1, num_blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
 
     def forward(self, x):
-        hidden = None
+        x = self.conv1(x)
+        x = self.maxpool1(x)
+        x = self.resblock1(x)
+        x = self.resblock2(x)
+        x = self.resblock3(x)
+        x = self.resblock4(x)
+        x = self.avgpool(x)
+        x = self.flat(x)
+        x = self.softmax(self.fullcon(x))
 
-        x = self.relu1(self.conv1(x))
-        x = self.flatten(x)
-        x = self.soft(self.lin1(x))
-        
         return x
